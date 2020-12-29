9f7d8727e2811310df9ac0f82607cc46e07820bb
Elizaveta Tishkovets <tiishka2@gmail.com>
2020-12-29 19:04:27 +0300

    Part 15: Improvement project structure

--- app/models.py
+++ app/models.py
@@ -3,9 +3,10 @@ from hashlib import md5
 from time import time
 
 import jwt
+from flask import current_app
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db, login, app
+from app import db, login
 
 followers = db.Table('followers',
                      db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
@@ -61,7 +62,7 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+            current_app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
 
     @staticmethod
     def verify_reset_password_token(token):
--- app/translate.py
+++ app/translate.py
@@ -1,17 +1,17 @@
 import json
 import requests
+from flask import current_app
 from flask_babel import _
-from app import app
 
 
 def translate(text, dest_language):
-    if 'IAM_TOKEN' not in app.config or not app.config['IAM_TOKEN']:
+    if 'IAM_TOKEN' not in current_app.config or not current_app.config['IAM_TOKEN']:
         return _('Error: the translation service is not configured.')
 
     url = "https://translate.api.cloud.yandex.net/translate/v2/translate"
     headers = {
         'Content-type': 'application/json',
-        'Authorization': f"Bearer {app.config['IAM_TOKEN']}"
+        'Authorization': f"Bearer {current_app.config['IAM_TOKEN']}"
     }
     data = {
         "folder_id": "b1gdqll8rul9h2n8bq6l",
