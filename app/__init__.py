c42abfdedc8f617fa7a8869630efa0b8e7129010
Elizaveta <tiishka2@gmail.com>
2021-01-11 16:32:55 +0300

    Part 21: Notifications

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
+
+from elasticsearch import Elasticsearch
 from flask import Flask, request, current_app
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -9,7 +11,6 @@ from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
-from elasticsearch import Elasticsearch
 from config import Config
 
 db = SQLAlchemy()
@@ -89,4 +90,3 @@ def get_locale():
     return request.accept_languages.best_match(current_app.config['LANGUAGES'])
 
 
-from app import models
--- microblog.py
+++ microblog.py
@@ -1,6 +1,5 @@
 from app import db, cli, create_app
-from app.models import User, Post
-
+from app.models import User, Post, Notification, Message
 
 app = create_app()
 cli.register(app)
@@ -8,4 +7,5 @@ cli.register(app)
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    return {'db': db, 'User': User, 'Post': Post, 'Message': Message,
+            'Notification': Notification}
